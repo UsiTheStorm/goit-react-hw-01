1f50035f76acca6dedb281a5b8d837391a3781e3
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-06-08 17:05:50 +0100

    Add user data file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 import Profile from './components/Profile/Profile';
 
+import './App.css';
+import userData from './userData.json';
+
+console.log(userData);
+
 function App() {
   return (
     <div>
