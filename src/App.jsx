b66bd02387ce6c7bcdd81d0871c2b346b0445a51
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-06-09 14:58:08 +0100

    Import profie component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
 import Profile from './components/Profile/Profile';
+import FriendList from './components/FriendList/FriendList';
 
 import './App.css';
-import userData from './userData.json';
+import userData from './data/userData.json';
 
 // console.log(userData);
 
 function App() {
   return (
     <div>
+      <h2>User profile</h2>
       <Profile user={userData} />
+      <h2>Friends list</h2>
+      <FriendList />
     </div>
   );
 }
