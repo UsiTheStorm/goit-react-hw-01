4c7859cf3360965fb91abc3c26212f3483ad4f17
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-06-10 11:16:22 +0100

    Add profile styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -19,15 +19,15 @@ function Profile({
       </div>
 
       <ul className={styles.stats}>
-        <li>
+        <li className={styles.statsItem}>
           <span className={styles.statsLabel}>Followers</span>
           <span className={styles.statsValue}>{followers}</span>
         </li>
-        <li>
+        <li className={styles.statsItem}>
           <span className={styles.statsLabel}>Views</span>
           <span className={styles.statsValue}>{views}</span>
         </li>
-        <li>
+        <li className={styles.statsItem}>
           <span className={styles.statsLabel}>Likes</span>
           <span className={styles.statsValue}>{likes}</span>
         </li>
